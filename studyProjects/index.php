4415561cc20381a32c280b2d3faa015ac86d3679
unknown <vinod@devbrother.com>
2012-07-09 00:29:31 +0530

    changes made in de branch

--- studyProjects/index.php
+++ studyProjects/index.php
@@ -5,7 +5,7 @@ $second_number = 20;
 $sum_total = $first_number + $second_number;
 
 $direct_text = 'The two variables added together = ';
-
+//this comment is added using dev branch..
 print ($direct_text . $sum_total);
 
 ?>
