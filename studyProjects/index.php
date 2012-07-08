48e625ddfd5f2c2effcdfdc8b59e639fdc1dbed9
unknown <vinod@devbrother.com>
2012-07-08 23:03:17 +0530

    Adding all the pfp files

---
+++ studyProjects/index.php
@@ -0,0 +1,11 @@
+<?php
+
+$first_number = 10;
+$second_number = 20;
+$sum_total = $first_number + $second_number;
+
+$direct_text = 'The two variables added together = ';
+
+print ($direct_text . $sum_total);
+
+?>
---
+++ studyProjects/sample.php
@@ -0,0 +1,11 @@
+<?php
+
+$first_number = 10;
+$second_number = 20;
+$prod_total = $first_number * $second_number;
+
+$direct_text = 'The two variables added together = ';
+
+print ($direct_text . $prod_total);
+
+?>
